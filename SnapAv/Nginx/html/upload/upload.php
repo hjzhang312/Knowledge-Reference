f90dd65792e025dee78ba052e9aa32d333cdbe97
hjzhang312 <hjzhang312@163.com>
2022-01-20 18:41:39 -0800

    增加Snapav的html文件配置

---
+++ SnapAv/Nginx/html/upload/upload.php
@@ -0,0 +1,24 @@
+
+<?php
+if ($_FILES["file"]["error"] > 0)
+  {
+	  #echo "Error: " . $_FILES["file"]["error"] . "<br />";
+	$webret['errorCode']= '1000';
+	$webret['errorMsg'] =  $_FILES["file"]["error"];
+	echo json_encode($webret);
+
+  }
+else
+  {
+	$webret['errorCode'] = '0000';
+	$webret['name'] = $_FILES["file"]["name"];
+	$webret['type'] = $_FILES["file"]["type"];
+	$webret['size'] = $_FILES["file"]["size"];
+	$webret['tmpIn'] = $_FILES["file"]["tmp_name"];
+	$webret['stored'] =  "/tmp/" . $_FILES["file"]["name"];
+
+move_uploaded_file($_FILES["file"]["tmp_name"], "/tmp/" . $_FILES["file"]["name"]);
+	echo json_encode($webret);
+
+  }
+?>
